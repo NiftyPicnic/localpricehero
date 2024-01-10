7c0b5f5671a0b7c1fa44f3152bda84133b1a4f96
User <user@Users-MacBook-Pro.local>
2024-01-10 13:39:55 +0000

    Save 10/01/23

--- my-vue-project/src/components/SlotMachineComponent.vue
+++ my-vue-project/src/components/SlotMachineComponent.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="slot-machine">
     <div class="reel">
-      <div class="variable">Location: {{ selectedLocation }}</div>
-      <select v-model="selectedLocation">
-        <option v-for="location in locations" :key="location" :value="location">
-          {{ location }}
-        </option>
-      </select>
+      <div class="arrow up" @mouseenter="startScroll('location', 'up')" @mouseleave="stopScroll">
+        &uarr;
+      </div>
+      <div class="variable location">{{ selectedLocation }}</div>
+      <div class="arrow down" @mouseenter="startScroll('location', 'down')" @mouseleave="stopScroll">
+        &darr;
+      </div>
     </div>
 
     <div class="reel">
-      <div class="variable">Size: {{ selectedSize }}</div>
-      <select v-model="selectedSize">
-        <option v-for="size in sizes" :key="size" :value="size">
-          {{ size }}
-        </option>
-      </select>
+      <div class="arrow up" @mouseenter="startScroll('size', 'up')" @mouseleave="stopScroll">
+        &uarr;
+      </div>
+      <div class="variable size">{{ selectedSize }}</div>
+      <div class="arrow down" @mouseenter="startScroll('size', 'down')" @mouseleave="stopScroll">
+        &darr;
+      </div>
     </div>
 
     <div class="reel">
-      <div class="variable">Price: {{ computedPrice }}</div>
+      <div class="variable price">{{ computedPrice }}</div>
     </div>
   </div>
 </template>
@@ -34,7 +36,10 @@ export default {
       sizes: [],
       selectedLocation: null,
       selectedSize: null,
-      prices: {}
+      prices: {},
+      scrollDirection: null,
+      scrollInterval: null,
+      scrollSpeed: 200, // Adjusted scroll speed
     };
   },
   computed: {
@@ -55,6 +60,7 @@ export default {
         this.locations = response.data.locations;
         this.sizes = response.data.sizes;
         this.prices = this.buildPriceLookup(response.data.prices);
+        this.selectRandomValues();
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -65,6 +71,46 @@ export default {
         prices[item.location] = item.prices;
       });
       return prices;
+    },
+    startScroll(variable, direction) {
+      this.scrollDirection = direction;
+      this.scrollInterval = setInterval(() => {
+        let selectedValue;
+        if (variable === 'location') {
+          selectedValue = this.selectedLocation;
+        } else {
+          selectedValue = this.selectedSize;
+        }
+        const currentIndex = variable === 'location' ? this.locations.indexOf(selectedValue) : this.sizes.indexOf(selectedValue);
+        if (currentIndex !== -1) {
+          let newIndex;
+          if (this.scrollDirection === 'up') {
+            newIndex = currentIndex - 1;
+            if (newIndex < 0) {
+              newIndex = variable === 'location' ? this.locations.length - 1 : this.sizes.length - 1;
+            }
+          } else {
+            newIndex = currentIndex + 1;
+            if (newIndex >= (variable === 'location' ? this.locations.length : this.sizes.length)) {
+              newIndex = 0;
+            }
+          }
+          if (variable === 'location') {
+            this.selectedLocation = this.locations[newIndex];
+          } else {
+            this.selectedSize = this.sizes[newIndex];
+          }
+        }
+      }, this.scrollSpeed);
+    },
+    stopScroll() {
+      clearInterval(this.scrollInterval);
+    },
+    selectRandomValues() {
+      const randomLocationIndex = Math.floor(Math.random() * this.locations.length);
+      const randomSizeIndex = Math.floor(Math.random() * this.sizes.length);
+      this.selectedLocation = this.locations[randomLocationIndex];
+      this.selectedSize = this.sizes[randomSizeIndex];
     }
   }
 };
@@ -81,18 +127,27 @@ export default {
 .reel {
   text-align: center;
   padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  flex: 1; /* Equal distribution of space */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .variable {
   font-size: 18px;
   margin-bottom: 10px;
+  margin-top: 10px; /* Adjusted margin-top to move the content down a bit */
 }
 
-select {
-  width: 100%;
-  padding: 5px;
-  font-size: 16px;
+.arrow {
+  width: 30px;
+  height: 30px;
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 20px;
 }
 </style>
